1db3ad6b528c93da3c2e83318af7c92d0d350816
Arbaz <arbazshaikh1507@gmail.com>
2023-10-11 14:55:34 +0530

    Instruction Set

---
+++ script.js
@@ -0,0 +1,25 @@
+
+let button = document.getElementById('start');
+let nextStep = button.addEventListener("click" , nextpage);
+let count = 0;
+function nextpage(){
+    let checkSelect = document.getElementById('accept').checked;
+    console.log("Hello");
+    if(checkSelect){
+        location.replace("./index.html");
+    }
+    else{
+        let element = document.createElement('h3');
+        element.innerHTML = "Checkbox Is Not Checked";
+        element.id = 'resp';
+        element.style.textAlign = 'center';
+        document.body.appendChild(element);
+    }
+    if(count == 1){
+    let ele = document.getElementById('resp');
+    document.body.removeChild(ele);
+    count = 0;
+    }
+    count++;
+}
+
